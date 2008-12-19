55c507740d2fb4f67952950b0c3ee537c0521990
anupom.syam <anupom.syam@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-19 15:44:13 +0000

    #77

--- app/app_model.php
+++ app/app_model.php
@@ -3,6 +3,8 @@
  * Application model for Cake.
  */
 class AppModel extends Model{
+	var $memcache = null;
+	
     // var cacheQueries = false;
 	
 	/**
@@ -29,5 +31,33 @@ class AppModel extends Model{
          */
         return true;
     }
+	
+	
+	function mc_connect() {
+		$this->memcache = new Memcache();
+		$this->memcache->connect('localhost', 11211) or die ("Could not connect");
+	}
+	
+	function mc_get($str, $postfix = false) {
+		return $this->memcache->get($this->__get_mc_key($str, $postfix));
+	}
+	
+	//$ttl = mins
+	function mc_set($str, $value, $postfix = false, $ttl = 0) {
+		$ttl = $ttl * 60;
+		return $this->memcache->set($this->__get_mc_key($str, $postfix), $value, false, $ttl) or die ("Failed to save data at the server");
+	}
+	
+	function mc_close() {
+		return $this->memcache->close();
+	}
+	
+	function __get_mc_key($str, $postfix) {
+		$key = MEMCACHE_PREFIX .'-'. $str;
+		if($postfix) {
+			$key .= '-'.$postfix;
+		}
+		return $key;
+	}
 }
 ?>
