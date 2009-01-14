4dd42eff49ba446717ed0a6ff869faf0eeb8b762
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-14 14:43:49 +0000

    update comments reply listing

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -70,6 +70,11 @@ $this->params['webservices']
  To enable/disable to write debug log for service controller.Value 1 for enable and 0 for desable
  */
  define("WRITE_LOG", 1);
+ 
+ /**
+ Set how many comment reply to be show.
+ */
+ define("NUM_COMMENT_REPLY", 4);
 
  /**
   * sets the time interval (hrs) in which
