63a18efae2d94239fcac6790b7fd5dfcc6dcc1ed
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-21 06:21:06 +0000

    #43:add link for share_project

--- app/config/routes.php
+++ app/config/routes.php
@@ -78,6 +78,7 @@ Router::connect('/users/removefavorites', array('controller' => 'users', 'action
 Router::connect('/users/*', array('controller' => 'users', 'action' => 'view'));
 
 // Project pages
+Router::connect('/projects/share_project', array('controller' => 'projects', 'action' => 'share_project'));
 Router::connect('/projects/gallerylist/*', array('controller' => 'projects', 'action' => 'gallerylist'));
 Router::connect('/projects/feature/*', array('controller' => 'projects', 'action' => 'feature'));
 Router::connect('/projects/defeature/*', array('controller' => 'projects', 'action' => 'defeature'));
