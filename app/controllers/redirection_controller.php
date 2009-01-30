34b425c568806ee4fd06a2215c3ff2608ed5b49e
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-30 14:22:13 +0000

    test #149

---
+++ app/controllers/redirection_controller.php
@@ -0,0 +1,48 @@
+<?php
+class RedirectionController extends Controller {
+var $components = array('Cookie');
+var $uses =array();
+
+function about(){
+	$lang_array =array();
+	$cookie_lang = $this->Cookie->read(lang);
+	$pos=strpos($cookie_lang,'-');
+	if($pos===false){
+		$lang = $cookie_lang;
+	}
+	else
+	{
+	 	$lang_array =explode('-',$cookie_lang);
+	 	$lang = $lang_array['0'];
+	
+	}
+	if($lang=='en')
+	$this->redirect('http://info.scratch.mit.edu/About_Scratch');
+	else
+	$this->redirect('http://info.scratch.mit.edu/About_Scratch/'.$lang);
+}//about fun
+
+
+function support(){
+	$lang_array =array();
+	$cookie_lang = $this->Cookie->read(lang);
+	$pos=strpos($cookie_lang,'-');
+	if($pos===false){
+		$lang = $cookie_lang;
+	}
+	else
+	{
+	 	$lang_array =explode('-',$cookie_lang);
+	 	$lang = $lang_array['0'];
+	
+	}
+	if($lang =='en')
+	$this->redirect('http://info.scratch.mit.edu/Support');
+	else
+	$this->redirect('http://info.scratch.mit.edu/Support/'.$lang);
+}//about support
+
+
+
+}//class
+?>
