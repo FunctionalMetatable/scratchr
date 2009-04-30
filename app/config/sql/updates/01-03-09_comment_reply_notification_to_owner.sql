02950a3d9929e2d1ae07ba47ae224243ce28e90c
anupom.syam <anupom.syam@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-04-30 22:25:17 +0000

    updating database structure

--- app/config/sql/updates/01-03-09_comment_reply_notification_to_owner.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `notification_types` (`type`, `template`, `is_admin`) VALUES
- ('new_pcomment_reply_to_owner', '<a href="/users/{from_user_name}">{from_user_name}</a> replied to a comment on your project <a href="/projects/{project_owner_name}/{project_id}">{project_name}</a>', '0'),
-('new_gcomment_reply_to_owner', '<a href="/users/{from_user_name}">{from_user_name}</a> replied to a comment on your gallery <a href="/galleries/view/{gallery_id}">{gallery_name}</a>', '0');
--- app/config/sql/updates/12-19-08_temp_user_block.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` ADD `blocked_till` TIMESTAMP NOT NULL AFTER `status`;
