3643ed7640fdb6ae0ca22a5cec0c07320defc3cb
anupom.syam <anupom.syam@media.mit.edu>
2009-04-30 22:25:17 +0000

    updating database structure

--- app/config/sql/updates/12-09-08_admin_notification_mechanism.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `notification_types` ADD `is_admin` BINARY NOT NULL DEFAULT '0';
-
-ALTER TABLE `notification_types` ADD INDEX ( `is_admin` ) ;
-
-INSERT INTO `notification_types` (`id` , `type` , `template` , `is_admin` ) VALUES ( NULL , 'blank', '', '1' );
