2c98d01a2a51ad5df0146db96c1c7f45f34803af
anupom.syam <anupom.syam@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-05-22 14:13:58 +0000

    #altering fieldnames projects.related_project_id to projects.based_on_pid # dropping projects.related_username #modified codes to reflect this db changes

--- app/config/sql/updates/05-20-2009_altering_projects.related_star.sql
+++ app/config/sql/updates/05-20-2009_altering_projects.related_star.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `projects` CHANGE `related_project_id` `based_on_pid` INT( 10 ) UNSIGNED NULL DEFAULT NULL ,
-CHANGE `related_username` `based_on_uid` INT( 10 ) UNSIGNED NULL DEFAULT NULL 
+ALTER TABLE `projects` CHANGE `related_project_id` `based_on_pid` INT( 10 ) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `projects` DROP `related_username`;
