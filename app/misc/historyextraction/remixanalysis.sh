1c208a50a31bb88d59e1c5788bd6253b8e200104
andresmh <andresmh@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-06-07 23:44:10 +0000

    rita's scripts

---
+++ app/misc/historyextraction/remixanalysis.sh
@@ -0,0 +1,1 @@
+java -Xms500M -Xmx500M -jar remixanalyzer.jar "scratchdb.media.mit.edu:3306/beta?user=beta&password=20ames" "remix_history_again" "/llk/scratchr/projects" -p -b "2009-05-29 11:59:59" -l > again_tryingall-2009-05-29-11-59-59.log
