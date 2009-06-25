afe4e4f3a1e3f28191cd3fd2b093f003853a4f90
anupom.syam <anupom.syam@media.mit.edu>
2009-06-25 17:04:01 +0000

    moving scanner to its own directory

---
+++ app/misc/scanner/config.php
@@ -0,0 +1,10 @@
+<?php
+define('SERVER', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DATABASE', 'scratchr_dev');
+define('APP', '/var/www/scratch/app/');
+define('JAVA_PATH', 'java');
+define('PROJECT_PATH', '/var/www/scratch/app/webroot/static/projects/');
+define('EMAIL', 'user@example.com');
+?>
