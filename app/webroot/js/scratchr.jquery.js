1eb75696479bae9fccf31a04f5158048478ff2cf
syamantics <syamantics@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-11-04 15:14:30 +0000

    test #299

---
+++ app/webroot/js/scratchr.jquery.js
@@ -0,0 +1,14 @@
+//the div which contains the login form
+var toggled = false;
+
+function showLogin() {
+    if(toggled) { //visible
+        $('#logincontainer').fadeOut('medium', null)
+        toggled = false;
+    }
+    else {
+        $('#logincontainer').fadeIn('slow',
+                function() { $('#UserInput').focus() } );
+        toggled = true;
+    }
+}
