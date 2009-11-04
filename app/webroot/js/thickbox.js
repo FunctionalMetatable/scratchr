2a1ba998816e051709793a266b2600d05a11bc14
syamantics <syamantics@media.mit.edu>
2009-11-04 15:14:30 +0000

    test #299

--- app/webroot/js/thickbox.js
+++ app/webroot/js/thickbox.js
@@ -7,14 +7,14 @@
  */
 
 //on page load call TB_init
-jQuery(document).ready(TB_init);
+$(document).ready(TB_init);
 
 var TB_NextObjToShow, TB_NextDirection, TB_WIDTH = 0, TB_HEIGHT = 0, TB_VisibleSelects, TB_WasOpen;
 
 //add thickbox to href elements that have a class of .thickbox
 function TB_init(){
-	jQuery("a.thickbox").click(function(){
-        //var t = this.title || this.innerHTML || this.href;
+	$("a.thickbox").click(function(){
+		//var t = this.title || this.innerHTML || this.href;
 		//TB_show(t,this.href);
 		TB_ShowObj(this);
 		this.blur();
@@ -27,9 +27,9 @@ function TB_getPrevObj(ThickObj) {
 	var PrevObj = null;
 	var url = ThickObj.href;
 	var thickgroup = ThickObj.rel;
-	
-	if (thickgroup != "") {	
-		TB_ObjSet = jQuery("a.thickbox[@rel=" + thickgroup + "]")
+
+	if (thickgroup != "") {
+		TB_ObjSet = $("a.thickbox[@rel=" + thickgroup + "]")
 		TB_ObjSize = TB_ObjSet.size();
 		for (var TB_Counter = 0; TB_Counter < TB_ObjSize; TB_Counter++) {
 			if (TB_ObjSet.get(TB_Counter) == url) {
@@ -39,7 +39,7 @@ function TB_getPrevObj(ThickObj) {
 			}
 		}
 	}
-	
+
 	return PrevObj;
 }
 
@@ -48,9 +48,9 @@ function TB_getNextObj(ThickObj) {
 	var url = ThickObj.href;
 	var thickgroup = ThickObj.rel;
 	var FoundThickObj = 0;
-	
-	if (thickgroup != "") {	
-		TB_ObjSet = jQuery("a.thickbox[@rel=" + thickgroup + "]")
+
+	if (thickgroup != "") {
+		TB_ObjSet = $("a.thickbox[@rel=" + thickgroup + "]")
 		TB_ObjSize = TB_ObjSet.size();
 		for (var TB_Counter = 0; TB_Counter < TB_ObjSize; TB_Counter++) {
 			if (TB_ObjSet.get(TB_Counter) == url) {
@@ -63,7 +63,7 @@ function TB_getNextObj(ThickObj) {
 			}
 		}
 	}
-	
+
 	return NextObj;
 }
 
@@ -74,29 +74,29 @@ function TB_ShowObj(ThickObj) {
 		caption = ThickObj.title || ThickObj.name || "";
 		url = ThickObj.href;
 		thickgroup = ThickObj.rel;
-	
+
 		if (document.getElementById("TB_overlay") == null) {
-			jQuery("body").append("<div id='TB_overlay'></div><div id='TB_window'></div>");
-			jQuery("#TB_overlay").click(TB_remove);
+			$("body").append("<div id='TB_overlay'></div><div id='TB_window'></div>");
+			$("#TB_overlay").click(TB_remove);
 		}
-		jQuery(window).resize(TB_position);
-		jQuery(window).scroll(TB_position);
-		
+		$(window).resize(TB_position);
+		$(window).scroll(TB_position);
+
 		TB_PrevObj = TB_getPrevObj(ThickObj);
 		if (TB_PrevObj != null) {
 			TB_PrevHTML = "<div id='TB_prev'><a href='#'>&lt;&lt; Prev</a></div>";
 		} else {
-			TB_PrevHTML = "";				
+			TB_PrevHTML = "";
 		}
 		TB_NextObj = TB_getNextObj(ThickObj);
 		if (TB_NextObj != null) {
 			TB_NextHTML = "<div id='TB_next'><a href='#'>Next &gt;&gt;</a></div>";
 		} else {
-			TB_NextHTML = "";				
+			TB_NextHTML = "";
 		}
- 		
-		//jQuery("#TB_overlay").show();
-		jQuery("body").append("<div id='TB_load'><div id='TB_loadContent'><img src='images/circle_animation.gif' /></div></div>");
+
+		//$("#TB_overlay").show();
+		$("body").append("<div id='TB_load'><div id='TB_loadContent'><img src='images/circle_animation.gif' /></div></div>");
 		var urlString = /\.jpg|\.jpeg|\.png|\.gif|\.mpg|\.mpeg|\.avi|\.html|\.htm|\.php|\.cfm|\.asp|\.aspx|\.jsp|\.jst|\.rb|\.txt/g;
 		var urlType = url.toLowerCase().match(urlString) + '';
 		switch (urlType) {
@@ -113,18 +113,18 @@ function TB_ShowObj(ThickObj) {
 					var imageWidth = imgPreloader.width;
 					var imageHeight = imgPreloader.height;
 					if (imageWidth > x) {
-						imageHeight = imageHeight * (x / imageWidth); 
-						imageWidth = x; 
-						if (imageHeight > y) { 
-							imageWidth = imageWidth * (y / imageHeight); 
-							imageHeight = y; 
+						imageHeight = imageHeight * (x / imageWidth);
+						imageWidth = x;
+						if (imageHeight > y) {
+							imageWidth = imageWidth * (y / imageHeight);
+							imageHeight = y;
 						}
 					} else {
-						if (imageHeight > y) { 
-							imageWidth = imageWidth * (y / imageHeight); 
-							imageHeight = y; 
-							if (imageWidth > x) { 
-								imageHeight = imageHeight * (x / imageWidth); 
+						if (imageHeight > y) {
+							imageWidth = imageWidth * (y / imageHeight);
+							imageHeight = y;
+							if (imageWidth > x) {
+								imageHeight = imageHeight * (x / imageWidth);
 								imageWidth = x;
 							}
 						}
@@ -132,24 +132,24 @@ function TB_ShowObj(ThickObj) {
 					// End Resizing
 					TB_WIDTH = imageWidth; // +60
 					TB_HEIGHT = imageHeight; // +80
-					jQuery("#TB_window").append("<div id='TB_caption'>"+caption+"</div><div id='TB_closeWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>" + TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ImageDIV'><a href='' id='TB_ImageOff' title='Close'><img id='TB_Image' src='"+url+"' width='"+imageWidth+"' height='"+imageHeight+"' alt='"+caption+"' onload='TB_ShowBox();'/></a></div>");
+					$("#TB_window").append("<div id='TB_caption'>"+caption+"</div><div id='TB_closeWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>" + TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ImageDIV'><a href='' id='TB_ImageOff' title='Close'><img id='TB_Image' src='"+url+"' width='"+imageWidth+"' height='"+imageHeight+"' alt='"+caption+"' onload='TB_ShowBox();'/></a></div>");
 
-					jQuery("#TB_closeWindowButton").click(TB_remove);
+					$("#TB_closeWindowButton").click(TB_remove);
 					if (!(TB_PrevHTML == "")) {
-						jQuery("#TB_prev").click(function () {
+						$("#TB_prev").click(function () {
 							TB_HideBox("r", TB_PrevObj);
 						});
 					}
 					if (!(TB_NextHTML == "")) {
-						jQuery("#TB_next").click(function () {
+						$("#TB_next").click(function () {
 							TB_HideBox("l", TB_NextObj);
 						});
 					}
 					TB_position();
-					jQuery("#TB_load").remove();
-					jQuery("#TB_ImageOff").click(TB_remove);
+					$("#TB_load").remove();
+					$("#TB_ImageOff").click(TB_remove);
 				}
-		  
+
 				imgPreloader.src = url;
 				break;
 			case ".mpg":
@@ -161,48 +161,48 @@ function TB_ShowObj(ThickObj) {
 				TB_HEIGHT = (params['height']*1); // +80
 				ajaxContentW = TB_WIDTH - 30;
 				ajaxContentH = TB_HEIGHT - 45;
-				jQuery("#TB_window").append("<div id='TB_caption'>"+caption+"</div><div id='TB_closeWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>" + TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ImageDIV' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'><a href='' id='TB_ImageOff' title='Close'><embed id='TB_Movie' src='" + url + "' autostart='true'></embed></div>");
+				$("#TB_window").append("<div id='TB_caption'>"+caption+"</div><div id='TB_closeWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>" + TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ImageDIV' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'><a href='' id='TB_ImageOff' title='Close'><embed id='TB_Movie' src='" + url + "' autostart='true'></embed></div>");
 
-				jQuery("#TB_closeWindowButton").click(TB_remove);
+				$("#TB_closeWindowButton").click(TB_remove);
 				if (!(TB_PrevHTML == "")) {
-					jQuery("#TB_prev").click(function () {
+					$("#TB_prev").click(function () {
 						TB_HideBox("r", TB_PrevObj);
 					});
 				}
 				if (!(TB_NextHTML == "")) {
-					jQuery("#TB_next").click(function () {
+					$("#TB_next").click(function () {
 						TB_HideBox("l", TB_NextObj);
 					});
 				}
 				TB_position();
-				jQuery("#TB_load").remove();
-				jQuery("#TB_ImageOff").click(TB_remove);
+				$("#TB_load").remove();
+				$("#TB_ImageOff").click(TB_remove);
 				TB_ShowBox();
 				break;
 			default:
 				var queryString = url.replace(/^[^\?]+\??/,'');
 				var params = parseQuery( queryString );
-				
+
 				TB_WIDTH = (params['width']*1) + 30; // +60
 				TB_HEIGHT = (params['height']*1) + 40; // +80
 				ajaxContentW = TB_WIDTH - 30;
 				ajaxContentH = TB_HEIGHT - 45;
-				jQuery("#TB_window").append(TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'></div>" + "<div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>");
-				jQuery("#TB_closeWindowButton").click(TB_remove);
-				jQuery("#TB_ajaxContent").load(url, function(){
+				$("#TB_window").append(TB_PrevHTML + TB_NextHTML + "</div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'></div>" + "<div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>Close window</a></div><div id='TB_SecondLine'>");
+				$("#TB_closeWindowButton").click(TB_remove);
+				$("#TB_ajaxContent").load(url, function(){
 					TB_position();
-					jQuery("#TB_load").remove();
+					$("#TB_load").remove();
 					if (!(TB_PrevHTML == "")) {
-						jQuery("#TB_prev").click(function () {
+						$("#TB_prev").click(function () {
 							TB_HideBox("r", TB_PrevObj);
 						});
 					}
 					if (!(TB_NextHTML == "")) {
-						jQuery("#TB_next").click(function () {
+						$("#TB_next").click(function () {
 							TB_HideBox("l", TB_NextObj);
 						});
 					}
-					jQuery("#TB_loadContent").show();
+					$("#TB_loadContent").show();
 					TB_ShowBox();
 				});
 				break;
@@ -213,10 +213,10 @@ function TB_ShowObj(ThickObj) {
 }
 
 function TB_ShowBox(Direction) {
-	jQuery("#TB_overlay").show();
-	TB_VisibleSelects = jQuery("select:visible");
+	$("#TB_overlay").show();
+	TB_VisibleSelects = $("select:visible");
 	TB_VisibleSelects.toggle();
-	if (jQuery().DropInLeft != undefined) {
+	if ($().DropInLeft != undefined) {
 		// Interface Elements for JQuery are included
 		if (Direction == undefined) {
 			if (TB_NextDirection == undefined) {
@@ -227,85 +227,85 @@ function TB_ShowBox(Direction) {
 		}
 		if (TB_WasOpen) {
 			if (Direction.toLowerCase() == "r") {
-				jQuery("#TB_window").DropInRight(250);
+				$("#TB_window").DropInRight(250);
 			} else {
-				jQuery("#TB_window").DropInLeft(250);
+				$("#TB_window").DropInLeft(250);
 			}
 		} else {
-			jQuery("#TB_window").Grow(150);
+			$("#TB_window").Grow(150);
 			TB_WasOpen = true;
 		}
 	} else {
 		// Interface Elements for JQuery are not included
-		jQuery("#TB_overlay").show();
-		jQuery("#TB_window").slideDown("normal");
+		$("#TB_overlay").show();
+		$("#TB_window").slideDown("normal");
 		TB_WasOpen = true;
 	}
 }
 
 function TB_AnimationComplete() {
-//		jQuery("#TB_overlay").show();
-		jQuery("#TB_window").fxReset();
+//		$("#TB_overlay").show();
+		$("#TB_window").fxReset();
 }
 
 function TB_HideBox(Direction, LocalNextObjToShow) {
 	TB_NextObjToShow = LocalNextObjToShow;
-	if (jQuery().DropOutRight != undefined) {
+	if ($().DropOutRight != undefined) {
 		// Interface Elements for JQuery are included
 		if (Direction == undefined) {
 			Direction = "l";
 		}
 		TB_NextDirection = Direction;
 		if (Direction.toLowerCase() == "r") {
-			jQuery("#TB_window").DropOutLeft(250, function() {
+			$("#TB_window").DropOutLeft(250, function() {
 				TB_HideBox_Part2();
 			});
 		} else {
-			jQuery("#TB_window").DropOutRight(250, function() {
+			$("#TB_window").DropOutRight(250, function() {
 				TB_HideBox_Part2();
 			});
 		}
 	} else {
 		// Interface Elements for JQuery are not included
-		jQuery("#TB_window").slideUp("slow");
+		$("#TB_window").slideUp("slow");
 		TB_HideBox_Part2();
 	}
 }
 
 function TB_HideBox_Part2() {
-	jQuery("#TB_load").remove();
-	jQuery("#TB_window").remove();
-	jQuery("body").append("<div id='TB_window'></div>");
+	$("#TB_load").remove();
+	$("#TB_window").remove();
+	$("body").append("<div id='TB_window'></div>");
 	TB_ShowObj(TB_NextObjToShow);
 }
 
 //helper functions below
 
 function TB_remove() {
-    if (jQuery().Shrink != undefined) {
-        // Interface Elements for JQuery are included
-		//jQuery("#TB_window").Fold(500, 20, function() {
-		//	jQuery('#TB_window,#TB_overlay').remove();
-		//	jQuery("#TB_load").remove();
+	if ($().Shrink != undefined) {
+		// Interface Elements for JQuery are included
+		//$("#TB_window").Fold(500, 20, function() {
+		//	$('#TB_window,#TB_overlay').remove();
+		//	$("#TB_load").remove();
 		//	TB_VisibleSelects.toggle();
 		//	TB_WasOpen = false;
 		//	return false;
 		//});
-		jQuery("#TB_window").Shrink(500, function() {
-			jQuery('#TB_window,#TB_overlay').remove();
-            jQuery("#TB_load").remove();
+		$("#TB_window").Shrink(500, function() {
+			$('#TB_window,#TB_overlay').remove();
+			$("#TB_load").remove();
 			TB_VisibleSelects.toggle();
 			TB_WasOpen = false;
 			return false;
 		});
 	} else {
 		// Interface Elements for JQuery are not included
-		jQuery("#TB_window").fadeOut("fast",function(){jQuery('#TB_window,#TB_overlay').remove();});
-		jQuery("#TB_load").remove();
+		$("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay').remove();});
+		$("#TB_load").remove();
 		TB_VisibleSelects.toggle();
 		TB_WasOpen = false;
 		return false;
-	
+
 	TB_WasOpen = false;
 	alert(TB_WasOpen);
 	}
@@ -313,20 +313,20 @@ function TB_remove() {
 
 function TB_position() {
 	var pagesize = getPageSize();
-  
-  	if (window.innerHeight && window.scrollMaxY) {	
+
+  	if (window.innerHeight && window.scrollMaxY) {
 		yScroll = window.innerHeight + window.scrollMaxY;
 	} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
 		yScroll = document.body.scrollHeight;
 	} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
 		yScroll = document.body.offsetHeight;
   	}
-	
+
 	var arrayPageScroll = getPageScrollTop();
-	
-	jQuery("#TB_window").css({width:TB_WIDTH+"px",height:TB_HEIGHT+"px",
+
+	$("#TB_window").css({width:TB_WIDTH+"px",height:TB_HEIGHT+"px",
 	left: ((pagesize[0] - TB_WIDTH)/2)+"px", top: (arrayPageScroll[1] + ((pagesize[1]-TB_HEIGHT)/2))+"px" });
-	jQuery("#TB_overlay").css("height",yScroll +"px");
+	$("#TB_overlay").css("height",yScroll +"px");
 
 }
 
@@ -355,7 +355,7 @@ function getPageScrollTop(){
 	} else if (document.body) {// all other Explorers
 		yScrolltop = document.body.scrollTop;
 	}
-	arrayPageScroll = new Array('',yScrolltop) 
+	arrayPageScroll = new Array('',yScrolltop)
 	return arrayPageScroll;
 }
 
@@ -363,7 +363,7 @@ function getPageSize(){
 	var de = document.documentElement;
 	var w = window.innerWidth || self.innerWidth || (de&&de.clientWidth) || document.body.clientWidth;
 	var h = window.innerHeight || self.innerHeight || (de&&de.clientHeight) || document.body.clientHeight;
-	
-	arrayPageSize = new Array(w,h) 
+
+	arrayPageSize = new Array(w,h)
 	return arrayPageSize;
 }
