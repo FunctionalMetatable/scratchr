2f98b48635973c1217a31797eb847255ad577a1c
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-14 10:11:34 +0000

    set country name based on ip at the time of upload project

---
+++ app/controllers/components/geo_ip.php
@@ -0,0 +1,31 @@
+<?php
+class GeoIpComponent extends Object {
+    function lookupIp($ip) {
+        vendor("geoipcity");
+
+        $gi = geoip_open("files/geolitecity.dat", GEOIP_STANDARD);
+        $result = geoip_record_by_addr($gi, $ip);
+        geoip_close($gi);
+        
+        return get_object_vars($result);
+    }
+	
+	function lookupCountryCode($ip){
+		
+		App::import('Vendor', 'Example', array('file' => 'GeoIp'.DS.'geoip.inc'));
+		// read GeoIP database
+		$handle = geoip_open("files/GeoIP.dat", GEOIP_STANDARD);
+		return geoip_country_code_by_addr($handle, $ip);
+	}
+    
+    function findIp() {
+      if(getenv("HTTP_CLIENT_IP"))
+        return getenv("HTTP_CLIENT_IP"); 
+      elseif(getenv("HTTP_X_FORWARDED_FOR"))
+        return getenv("HTTP_X_FORWARDED_FOR"); 
+      else 
+        return getenv("REMOTE_ADDR"); 
+    }
+}
+
+?> 
