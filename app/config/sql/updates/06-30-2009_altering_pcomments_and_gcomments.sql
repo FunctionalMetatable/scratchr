adeb3318def9061fd8fdc109b39080e5c59db8ba
bakytn <bakytn@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-22 04:00:04 +0000

    fixing SQL syntax errors

--- app/config/sql/updates/06-30-2009_altering_pcomments_and_gcomments.sql
+++ app/config/sql/updates/06-30-2009_altering_pcomments_and_gcomments.sql
@@ -1,7 +1,7 @@
- ALTER TABLE `gcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible' 
+ ALTER TABLE `gcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible' ;
 
-ALTER TABLE `pcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible'
+ALTER TABLE `pcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible';
 
 
  ALTER TABLE `pcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment', 'parentcommentcensored' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible';
- ALTER TABLE `gcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment', 'parentcommentcensored' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible'  
+ ALTER TABLE `gcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment', 'parentcommentcensored' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'visible'  ;
