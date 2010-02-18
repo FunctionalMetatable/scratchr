92d286c97658f8ad45614759ddbbfcf0b5466cf6
andresmh <andresmh@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-02-18 04:51:39 +0000

    Memcache project tag counts. Very expensive query

--- app/controllers/tags_controller.php
+++ app/controllers/tags_controller.php
@@ -85,10 +85,31 @@ Class TagsController extends AppController {
 						"direction"=> "DESC", "url"=>"/tags/view/" . $tag_name . "/" . $option);
 		}
 		
-		 $final_count_result=$this->Tag->query("SELECT COUNT(DISTINCT(project_id)) AS `count` FROM `project_tags` AS `ProjectTag` LEFT JOIN `users` AS `User` ON (`ProjectTag`.`user_id` = `User`.`id`) LEFT JOIN `projects` AS `Project` ON (`ProjectTag`.`project_id` = `Project`.`id`) WHERE ".$count_criteria);
+
+		 # This is an expensive query and it's done a lot therefore we ought to
+		 # memcache it
+		 $count_sql = "SELECT COUNT(DISTINCT(project_id)) AS `count` FROM `project_tags` AS `ProjectTag` LEFT JOIN `users` AS `User` ON (`ProjectTag`.`user_id` = `User`.`id`) LEFT JOIN `projects` AS `Project` ON (`ProjectTag`.`project_id` = `Project`.`id`) WHERE ".$count_criteria;
+
+		 $mc_key = md5($count_sql);
+
+		 $this->ProjectTag->mc_connect();
+
+		 $final_count = $this->ProjectTag->mc_get($mc_key);
+
+		 if($final_count === false) {
+
+		    $final_count_result = $this->Tag->query($count_sql);
 		
-		 $final_count=$final_count_result[0][0]['count'];
-				
+		    $final_count = $final_count_result[0][0]['count'];
+
+		    $ttl = TAG_PAGINATION_CACHE_TTL;
+
+		    $this->ProjectTag->mc_set($mc_key, $final_count, false, $ttl);
+			
+		 }
+
+		 $this->ProjectTag->mc_close();
+
 		list($order,$limit,$page) = $this->Pagination->init($this->Project->addVisCheck($final_criteria), Array(), $options, $final_count);
 		
         $mc_key = 'project_tag_'.$tag_id.'_'.$content_status.'_'. $order.'_'. $limit.'_'. $page;
