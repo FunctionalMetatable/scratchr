c47e89f8def490774419acc8bb28e7deac785d54
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-03-04 05:43:56 +0000

    test #371

--- app/controllers/tags_controller.php
+++ app/controllers/tags_controller.php
@@ -59,102 +59,57 @@ Class TagsController extends AppController {
     }
 	
     function view($tag_name, $option = "views") {
-	$this->autoRender = true;
-	$this->pageTitle = ___("Scratch | Projects tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
+		$this->autoRender = true;
+		$this->pageTitle = ___("Scratch | Projects tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
         $tag =  $this->Tag->find("name = '$tag_name'");
         if (empty($tag))
 		$this->cakeError('error404');
 		$content_status = $this->getContentStatus();
 		
-		$this->Pagination->show = 10;
 		$tag_id = $tag['Tag']['id'];
 		$final_criteria = "(Project.proj_visibility = 'visible' OR Project.proj_visibility = 'censbycomm' OR Project.proj_visibility = 'censbyadmin') AND ProjectTag.tag_id = $tag_id GROUP BY project_id";
-		$count_criteria = "(Project.proj_visibility = 'visible' OR Project.proj_visibility = 'censbycomm' OR Project.proj_visibility = 'censbyadmin') AND ProjectTag.tag_id = $tag_id";
 
         if ($content_status == "safe") {
 			$final_criteria = "Project.status = 'safe' AND " . $final_criteria; 
-			$count_criteria = "Project.status = 'safe' AND " . $count_criteria; 
+		}
+		
+		if ($option == "views") {
+			$order = 'Project.views DESC';
+		}
+		
+		if ($option == "loveits") {
+			$order = 'Project.loveitsuniqueip DESC';
+		}
+		
+		if ($option == "creation") {
+			$order = 'Project.created DESC';
 		}
 		
 		$this->Project->bindUser();
-		$this->ProjectTag->bindProject(); 
-		$this->ProjectTag->unbindTag();
+        $this->ProjectTag->bindProject(); 
+        $this->ProjectTag->unbindTag();
 		
-		$this->modelClass = "ProjectTag";
-
-		if ($option == "views") {
-			$options = Array("sortBy"=>"views", "sortByClass" => "Project", 
-						"direction"=> "DESC", "url"=>"/tags/view/" . $tag_name . "/" . $option);
-		}
-		if ($option == "title") {
-			$options = Array("sortBy"=>"name", "sortByClass" => "Project", 
-						"direction"=> "ASC", "url"=>"/tags/view/" . $tag_name . "/" . $option);
-		}
-		if ($option == "loveits") {
-			$options = Array("sortBy"=>"loveit", "sortByClass" => "Project", 
-						"direction"=> "DESC", "url"=>"/tags/view/" . $tag_name . "/" . $option);
-		}
-		if ($option == "creation") {
-			$options = Array("sortBy"=>"created", "sortByClass" => "Project", 
-						"direction"=> "DESC", "url"=>"/tags/view/" . $tag_name . "/" . $option);
-		}
-		
-
-		 # This is an expensive query and it's done a lot therefore we ought to
-		 # memcache it
-#		 $count_sql = "SELECT COUNT(DISTINCT(project_id)) AS `count` FROM `project_tags` AS `ProjectTag` LEFT JOIN `users` AS `User` ON (`ProjectTag`.`user_id` = `User`.`id`) LEFT JOIN `projects` AS `Project` ON (`ProjectTag`.`project_id` = `Project`.`id`) WHERE ".$count_criteria;
-
-#		 $mc_key = md5($count_sql);
-
-#		 $this->ProjectTag->mc_connect();
-
-#		 $final_count = $this->ProjectTag->mc_get($mc_key);
-
-#		 if($final_count === false) {
-
-#		    $final_count_result = $this->Tag->query($count_sql);
-		
-#		    $final_count = $final_count_result[0][0]['count'];
-
-#		    $ttl = TAG_PAGINATION_CACHE_TTL;
-
-#		    $this->ProjectTag->mc_set($mc_key, $final_count, false, $ttl);
-			
-#		 }
-
-#		 $this->ProjectTag->mc_close();
-
-		# Set the number of projects to view to 200
-		$final_count = 200;
-
-		list($order,$limit,$page) = $this->Pagination->init($this->Project->addVisCheck($final_criteria), Array(), $options, $final_count);
-		
-        $mc_key = 'project_tag_'.$tag_id.'_'.$content_status.'_'. $order.'_'. $limit.'_'. $page;
+		$limit = NUM_TAGED_PROJECT;
+		$mc_key = 'project_tag_'.$tag_id.'_'.$content_status.'_'. $option.'_'. $limit;
+		$ttl = TAGED_PROJECT_CACHE_TTL; 
         $this->ProjectTag->mc_connect();
         $tag_projects = $this->ProjectTag->mc_get($mc_key);
         if($tag_projects === false) {
-            $this->Project->unbindModel(
-                array('hasMany' => array('GalleryProject'))
-            );
-            $this->ProjectTag->unbindModel(
-                array('belongsTo' => array('User'))
-            );
-            $tag_projects = $this->ProjectTag->findAll($final_criteria, null, $order, $limit, $page, 2);
-            $tag_projects = $this->set_projects($tag_projects);
-            $ttl = TAG_PAGINATION_CACHE_TTL;
-            if($option == 'creation') { //for sort by creation date
-                $ttl = TAG_PAGINATION_CACHE_TTL_CREATION;
-            }
-            $this->ProjectTag->mc_set($mc_key, $tag_projects, false, $ttl);
-        }
-        $this->ProjectTag->mc_close();
-
-	$this->set('option', $option);
+			$this->Project->unbindModel(array('hasMany' => array('GalleryProject')));
+			$this->ProjectTag->unbindModel(array('belongsTo' => array('User')));
+			$this->ProjectTag->recursive = 2;	
+			$tag_projects = $this->ProjectTag->findAll($final_criteria, NULL , $order, NUM_TAGED_PROJECT);
+			$tag_projects = $this->set_projects($tag_projects);
+			$this->ProjectTag->mc_set($mc_key, $tag_projects, false, $ttl);
+		}
+		$this->ProjectTag->mc_close();
+		$this->set('option', $option);
         $this->set('tag_projects', $tag_projects);
-	$this->set('tag_name', $tag_name);
+		$this->set('tag_name', $tag_name);
         $this->set('tag', $tag);
         $this->render('projects');
     }
+	
 	
 	function view_galleries($tag_name, $option = "projects") {
 		$this->autoRender = true;
@@ -221,9 +176,9 @@ Class TagsController extends AppController {
 				}
 			}
 			//Ribbon feature
+			$image_name ='';
 			if(SHOW_RIBBON ==1){
 				$featured_time = $this->FeaturedProject->field('timestamp',array('project_id'=>$project['Project']['id']));
-				$image_name ='';
 				if(!empty($featured_time)){
 					$text =$this->convertDate($featured_time);
 					$image_name =$this->ribbonImageName($featured_time );
