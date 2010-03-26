b42436bf1ff5a38343565291dc065e8d6897226b
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-03-26 09:08:38 +0000

    test #391

--- app/controllers/galleries_controller.php
+++ app/controllers/galleries_controller.php
@@ -3191,5 +3191,19 @@ Class GalleriesController extends AppController {
 			  }
 			
 	}
+	
+	function set_country_code(){
+		$this->Project->recursive = -1;
+		$projects = $this->Project->findAll("Project.upload_ip IS NOT NULL AND (Project.country IS NULL OR Project.country ='')",'id, inet_ntoa(upload_ip) as ip, country');
+		foreach($projects as $project){
+			$upload_ip = $project['0']['ip'];
+			$countryName = $this->GeoIp->lookupCountryCode($upload_ip);
+			$this->Project->id = $project['Project']['id'];
+			$this->Project->saveField('country', $countryName);
+			$this->Project->id = false;
+		}
+		
+		echo 'done';exit;
+	}//function
 }
 ?>
