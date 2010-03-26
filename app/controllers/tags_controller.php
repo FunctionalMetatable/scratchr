f337cd7b44caba16fb9ddf88691e68eb19a6abe5
andresmh <andresmh@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-03-26 19:22:43 +0000

    Speed up tags last changed query

--- app/controllers/tags_controller.php
+++ app/controllers/tags_controller.php
@@ -78,25 +78,21 @@ Class TagsController extends AppController {
     }
 	
     function view($tag_name, $option = "views") {
-		$this->autoRender = true;
-		$this->pageTitle = ___("Scratch | Projects tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
-        $tag =  $this->Tag->find("name = '$tag_name'");
+
+	$this->autoRender = true;
+	$this->pageTitle = ___("Scratch | Projects tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
+	$tag =  $this->Tag->find("name = '$tag_name'");
+
         if (empty($tag) || $option ==='title')
 		$this->cakeError('error404');
 		$content_status = $this->getContentStatus();
 		
 		$tag_id = $tag['Tag']['id'];
-		//Listing deleted user 
-		  $this->loadModel('User');  
- 		  $deleted_users = $this->User->find('list', 
- 		                array('conditions' =>  "User.status = 'delbyadmin' OR User.status = 'delbyusr'", 
- 		                'fields' => 'id'));
-		  $deleted_users_id = implode(',' ,$deleted_users);
-		
-		$final_criteria = "(Project.proj_visibility = 'visible' OR Project.proj_visibility = 'censbycomm' OR Project.proj_visibility = 'censbyadmin') AND ProjectTag.tag_id = $tag_id AND Project.user_id NOT IN (".$deleted_users_id.") GROUP BY project_id";
+		$final_criteria = "(Project.proj_visibility = 'visible' OR Project.proj_visibility = 'censbycomm' OR Project.proj_visibility = 'censbyadmin') AND ProjectTag.tag_id = $tag_id GROUP BY project_id";
 
         if ($content_status == "safe") {
-			$final_criteria = "Project.status = 'safe' AND " . $final_criteria; 
+
+		$final_criteria = "Project.status = 'safe' AND " . $final_criteria; 
 		}
 		
 		if ($option == "views") {
@@ -106,21 +102,28 @@ Class TagsController extends AppController {
 		if ($option == "loveits") {
 			$order = 'Project.loveitsuniqueip DESC';
 		}
-		
+
+		##############################################################################################
+		# If we are sorting by the project creation date we want to speed up the 
+		# query by eliminating full table scan. Since creation date is related to id we are 
+		# gonna limit to last 500 projects
 		if ($option == "creation") {
 			$order = 'Project.created DESC';
+			$result = $this->Project->query("SELECT MAX(projects.id) AS maxid FROM projects");
+			$projectid_min = intval($result[0][0]['maxid']) - 500; 
+			$final_criteria = "Project.id >  $projectid_min AND " . $final_criteria;
 		}
 		
 		$this->Project->bindUser();
-        $this->ProjectTag->bindProject(); 
-        $this->ProjectTag->unbindTag();
+		$this->ProjectTag->bindProject(); 
+		$this->ProjectTag->unbindTag();
 		
 		$limit = NUM_TAGED_PROJECT;
 		$mc_key = 'project_tag_'.$tag_id.'_'.$content_status.'_'. $option.'_'. $limit;
 		$ttl = TAGED_PROJECT_CACHE_TTL; 
-        $this->ProjectTag->mc_connect();
-        $tag_projects = $this->ProjectTag->mc_get($mc_key);
-        if($tag_projects === false) {
+		$this->ProjectTag->mc_connect();
+		$tag_projects = $this->ProjectTag->mc_get($mc_key);
+		if($tag_projects === false) {
 			$this->Project->unbindModel(array('hasMany' => array('GalleryProject')));
 			$this->ProjectTag->unbindModel(array('belongsTo' => array('User')));
 			$this->ProjectTag->recursive = 2;	
@@ -130,27 +133,28 @@ Class TagsController extends AppController {
 		}
 		$this->ProjectTag->mc_close();
 		$this->set('option', $option);
-        $this->set('tag_projects', $tag_projects);
+		$this->set('tag_projects', $tag_projects);
 		$this->set('tag_name', $tag_name);
-        $this->set('tag', $tag);
-        $this->render('projects');
-    }
+		$this->set('tag', $tag);
+		$this->render('projects');
+     }
 	
 	
-	function view_galleries($tag_name, $option = "projects") {
-		$this->autoRender = true;
-		$this->pageTitle = ___("Scratch | Galleries tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
+     function view_galleries($tag_name, $option = "projects") {
+
+	$this->autoRender = true;
+	$this->pageTitle = ___("Scratch | Galleries tagged with", true) . " '" . htmlspecialchars($tag_name) . "'";
         $tag =  $this->Tag->find("name = '$tag_name'");
         if (empty($tag))
-            $this->cakeError('error404');
-		$content_status = $this->getContentStatus();
+	    $this->cakeError('error404');
+	    $content_status = $this->getContentStatus();
 		
-		$this->Pagination->show = 10;
-		$tag_id = $tag['Tag']['id'];
-		$criteria = Array("tag_id" => $tag['Tag']['id']);
-		$criteria = Array("ProjectTag.tag_id = $tag_id");
-		$final_criteria = "Gallery.total_projects > 0 AND Gallery.visibility = 'visible' AND GalleryTag.tag_id = $tag_id GROUP BY gallery_id";
-        $count_criteria = "Gallery.total_projects > 0 AND Gallery.visibility = 'visible' AND GalleryTag.tag_id = $tag_id";
+	    $this->Pagination->show = 10;
+	    $tag_id = $tag['Tag']['id'];
+	    $criteria = Array("tag_id" => $tag['Tag']['id']);
+	    $criteria = Array("ProjectTag.tag_id = $tag_id");
+	    $final_criteria = "Gallery.total_projects > 0 AND Gallery.visibility = 'visible' AND GalleryTag.tag_id = $tag_id GROUP BY gallery_id";
+	    $count_criteria = "Gallery.total_projects > 0 AND Gallery.visibility = 'visible' AND GalleryTag.tag_id = $tag_id";
 
 		if ($content_status == "safe") {
 			$final_criteria = "Gallery.status = 'safe' AND " . $final_criteria; 
