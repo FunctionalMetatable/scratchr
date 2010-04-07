0795850eb92ad618014b73b4cd8c9ff0374bb4fd
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-04-07 09:23:04 +0000

    test #391

---
+++ app/controllers/geo_ips_controller.php
@@ -0,0 +1,22 @@
+<?php
+class GeoIpsController extends AppController {
+    var $uses = array('Project');
+    var $components = array(); 
+    var $helpers = array(); 
+
+	function set_country_code(){
+		$this->Project->recursive = -1;
+		$projects = $this->Project->findAll("Project.upload_ip IS NOT NULL AND (Project.country IS NULL OR Project.country ='')",'id, inet_ntoa(upload_ip) as ip, country');
+		foreach($projects as $project){
+			$upload_ip = $project['0']['ip'];
+			$countryName = $this->GeoIp->lookupCountryCode($upload_ip);
+			$this->Project->id = $project['Project']['id'];
+			$this->Project->saveField('country', $countryName);
+			$this->Project->id = false;
+		}
+		
+		echo 'done';exit;
+	}//function
+}
+
+?>
