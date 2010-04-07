b6dced9a42896d7699f0ce97aa8bc06dae06e699
syamantics <syamantics@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-04-07 19:43:19 +0000

    test #391

--- app/controllers/geo_ips_controller.php
+++ app/controllers/geo_ips_controller.php
@@ -7,7 +7,7 @@ class GeoIpsController extends AppController {
         function set_country_code($page = 0) {
                 $this->Project->recursive = -1;
                 $projects = $this->Project->findAll("Project.upload_ip IS NOT NULL AND (Project.country IS NULL OR Project.country ='')",
-                                                'id, inet_ntoa(upload_ip) as ip, country', 'id DESC', 100, $page);
+                                                'id, inet_ntoa(upload_ip) as ip, country', 'id DESC', 100, $page. -1, 'all', 1);
                 echo 'processing page '.$page;
                 foreach($projects as $project){
                         $upload_ip = $project['0']['ip'];
