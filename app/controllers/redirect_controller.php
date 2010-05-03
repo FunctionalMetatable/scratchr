31e59f90acb021bf0876c9ac841072de78823abc
andresmh <andresmh@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-05-03 22:12:35 +0000

    fixed issue with some languages not being redirectly prpoperly, added array with map between scratch and drupal

--- app/controllers/redirect_controller.php
+++ app/controllers/redirect_controller.php
@@ -2,49 +2,36 @@
 class RedirectController extends Controller {
 var $components = array('Cookie');
 var $uses =array();
-
+var $scratchr2drupal = array(
+		'fr-fr' => 'fr',
+		'es-es' => 'es',
+		'ita'   => 'it',
+		'rus'	=> 'ru',
+		'dut'	=> 'nl',
+		'de-de' => 'de',
+		'heb'	=> 'he',
+		'jpn'	=> 'ja',
+		);
 function about(){
 	$lang_array =array();
 	$about_scratch_url = 'http://' . ABOUT_SCRATCH_URL;
 	$cookie_lang = $this->Cookie->read(lang);
-	$pos=strpos($cookie_lang,'-');
-	if($pos===false){
-		$lang = $cookie_lang;
-	}
-	else
-	{
-	 	$lang_array =explode('-',$cookie_lang);
-	 	$lang = $lang_array['0'];
-	
-	}
-	if($lang =='de' || $lang == 'es' || $lang == "it" || $lang == "fr" || $lang == 'he' || $lang == 'nl' || $lang == 'ja' || $lang == 'ru')
-		$this->redirect("$about_scratch_url/$lang/About_Scratch");
+	if($scratchr2drupal[$cookie_lang])
+		$this->redirect("$about_scratch_url/$scratchr2drupal[$cookie_lang]/About_Scratch");
 	else
 		$this->redirect("$about_scratch_url/About_Scratch");
-}
 
+}
 
 function support(){
 	$lang_array =array();
 	$support_url ='http://'.SUPPORT_URL;
 	$cookie_lang = $this->Cookie->read('lang');
-	$pos=strpos($cookie_lang,'-');
-	if($pos===false){
-		$lang = $cookie_lang;
-	}
+	if($scratchr2drupal[$cookie_lang])
+		$this->redirect("$about_scratch_url/$scratchr2drupal[$cookie_lang]/About_Scratch");
 	else
-	{
-	 	$lang_array =explode('-',$cookie_lang);
-	 	$lang = $lang_array['0'];
-	
-	}
-	if($lang =='de' || $lang == 'es' || $lang == "it" || $lang == "fr" || $lang == 'he' || $lang == 'nl' || $lang == 'ja' || $lang == 'ru')
-		$this->redirect("$support_url/$lang/Support");
-	else
-		$this->redirect("$support_url/Support");
-}//about support
+		$this->redirect("$about_scratch_url/About_Scratch");
+}
 
-
-
-}//class
+}
 ?>
