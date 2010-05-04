5da8e56dba1c706354392b96118c240d29fed527
andresmh <26597+andresmh@users.noreply.github.com>
2010-05-04 16:11:54 +0000

    fixes #348: this was fixed already but due to a mistake the change was reverted

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -897,7 +897,7 @@ class UsersController extends AppController {
 		
 		$user_status = trim($user_record['User']['status']);
 		if ($user_status == 'delbyadmin' || $user_status == 'delbyusr' || $user_status == 'locked') {
-			if( !$this->isAdmin() ) {
+			if( !$this->isLoggedIn() ) {
 				$this->cakeError('error',array('code'=>'404', 'message'=>'account_disabled', 'name' => __('Not Found', true)));
 			}
         }
