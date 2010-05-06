e01a10b7956c618868cd2687b5594526edc22f96
ashok123 <ashok123@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-05-06 06:32:41 +0000

    test #409

--- app/webroot/js/scratchr.jquery.js
+++ app/webroot/js/scratchr.jquery.js
@@ -14,6 +14,44 @@ function showLogin() {
     return false;
 }
 
-function hideUserCountryDiv() {
-	$('#userCountryDiv').fadeOut('medium', null);
+
+function hideUserCountryDiv(fast) {
+	
+
+	var userCountryDiv = document.getElementById("userCountryDiv");
+	if(typeof(fast) !== 'undefined') {
+		$('#userCountryDiv').hide();
+	}
+	else {
+		$('#userCountryDiv').fadeOut('medium', null);
+	}
+	setCookie('country_welcomed','1', 365);
+	return false;
 }
+
+function setCookie(name,value,days) {
+	if (days) 
+	{
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*3600000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else 
+		var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var prefix = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) 
+	{
+		var c = ca[i];
+		while (c.charAt(0)==' ') 
+			c = c.substring(1,c.length);
+		if (c.indexOf(prefix) == 0) 
+			return c.substring(prefix.length,c.length);
+	}
+	return null;
+}
+
