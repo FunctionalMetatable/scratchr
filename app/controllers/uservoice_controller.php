7f18c94905235c25cfad549010df715f5b871b91
Chinuaearl <Chinuaearl@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-06-28 21:52:46 +0000

    Added reasons must be more than 30 days old, must have 2 projects, and must have 10 comments on projects and/or galleries to go to suggestions.

--- app/controllers/uservoice_controller.php
+++ app/controllers/uservoice_controller.php
@@ -2,36 +2,51 @@
 class UservoiceController extends AppController {
    
    var $name = 'uservoice';
-   var $uses = array("Project","User","Pcomment");
+   var $uses = array("Project","User","Pcomment","Gcomment");
    
    // redirects the user to the page on uservoice with their login info
    // if the user is an experienced user, or to a page explaining why
    // they can't if it is a new scratcher.
    function index() {
-      $account_key = "scratch";
-      $api_key = "fc52e139e19cdb76957c674fa2a9f609";
-      $salted = $api_key . $account_key;
-      $hash = hash('sha1',$salted,true);
-      $saltedHash = substr($hash,0,16);
-      $iv = "OpenSSL for Ruby";
-      
-      $username = $this->getLoggedInUsername();
+    $account_key = "scratch";
+    $api_key = "fc52e139e19cdb76957c674fa2a9f609";
+    $salted = $api_key . $account_key;
+    $hash = hash('sha1',$salted,true);
+    $saltedHash = substr($hash,0,16);
+    $iv = "OpenSSL for Ruby";
+    
+    $username = $this->getLoggedInUsername();
+	$user_id = $this->getLoggedInUserID();
 
-      	// Decide whether to send user data and login to uservoice, or send user to sorry can't vote page.
+    // Decide whether to send user data and login to uservoice, or send user to sorry can't vote page.
 	// Eventually this should be done on basis of group membership (New Scratcher or Scratcher). 
 	// For now, let's make criteria:
 	// > 10 comments && > 2 projects && account age > 30 days.
-      if (!true)
-      	$this->redirect('http://info.scratch.mit.edu/Suggestions_New_Scratcher');
+	
+	$canSuggest = true;
+	$comment_count = $this->Pcomment->findCount(array('Pcomment.user_id' => $user_id,'comment_visibility'=>'visible')) + $this->Gcomment->findCount(array('Gcomment.user_id' => $user_id,'comment_visibility'=>'visible'));
+	if ($comment_count < 10)
+	$canSuggest = false;
+	$projects = $this->Project->find('all', array('conditions'=>array('Project.user_id' => $user_id, 'Project.proj_visibility'=>'visible'), 'fields'=> 'id', 'recursive'=> -1, 'order' =>'created DESC'));
+	if (count($projects) < 2)
+	$canSuggest = false;
+	$user = $this->User->read();
+	$created = preg_split("/[-\s:]+/",$user['created']);
+	$created = mktime($created[3],$created[4],$created[5],$created[1],$created[2],$created[0]);
+	$day_diff = (time() - $created)/86400;
+	if ($day_diff <30)
+	$canSuggest = false;
+	if (!$canSuggest)
+    $this->redirect('http://info.scratch.mit.edu/Suggestions_New_Scratcher');
 
-      // End test
+    // End test
 
-      $user_data = array(
-        "guid" => $this->getLoggedInUserID(),
+    $user_data = array(
+	    "guid" => $this->$user_id,
         "expires" => date('Y-m-d', strtotime('+1 year')),
         "display_name" => $username,
         "url" => 'http://scratch.mit.edu/users/'.$this->getLoggedInUrlname(),
-        "avatar_url" => 'http://scratch.mit.edu/static/icons/buddy/'.$this->getLoggedInUserID().'_med.png'
+        "avatar_url" => 'http://scratch.mit.edu/static/icons/buddy/'.$this->getLoggedInuser_id().'_med.png'
       );
 
       if ($this->isAdmin())
