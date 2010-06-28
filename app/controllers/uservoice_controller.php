673c15665c7c076807ec21668fdd8a425b522cb9
lightnin <lightnin@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-06-28 20:07:09 +0000

    Intermediate checkin. Works, but does not check #comments,projects,etc. before referring to uservoice.

--- app/controllers/uservoice_controller.php
+++ app/controllers/uservoice_controller.php
@@ -1,36 +1,28 @@
 <?php
-Class UservoiceController extends AppControler {
+class UservoiceController extends AppController {
    
-   Var $name = ‘uservoice’;
-   var $uses = array("Project","RelationshipType", "Relationship", "Gallery", "GalleryMembership", "User", "FriendRequest", "Notification");
+   var $name = 'uservoice';
+   var $uses = array("Project","User","Pcomment");
    
    // redirects the user to the page on uservoice with their login info
    // if the user is an experienced user, or to a page explaining why
-   // they can’t if it is a new scratcher.
+   // they can't if it is a new scratcher.
    function index() {
       $account_key = "scratch";
       $api_key = "fc52e139e19cdb76957c674fa2a9f609";
-      
       $salted = $api_key . $account_key;
       $hash = hash('sha1',$salted,true);
       $saltedHash = substr($hash,0,16);
       $iv = "OpenSSL for Ruby";
       
-      // Testing if user has made projects
+      $username = $this->getLoggedInUsername();
 
-      $username = $this->getLoggedInUsername()
-      
-      $ch = curl_init('http://scratch.mit.edu/api/getprojectsbyusername/'.$username);
-
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-      $has_projects = curl_exec($ch);
-      
-      curl_close($ch);
-
-
-      if (!trim($has_projects))
-      $this->redirect(“http http://info.scratch.mit.edu/Suggestions_New_Scratcher");
+      	// Decide whether to send user data and login to uservoice, or send user to sorry can't vote page.
+	// Eventually this should be done on basis of group membership (New Scratcher or Scratcher). 
+	// For now, let's make criteria:
+	// > 10 comments && > 2 projects && account age > 30 days.
+      if (!true)
+      	$this->redirect('http://info.scratch.mit.edu/Suggestions_New_Scratcher');
 
       // End test
 
@@ -39,15 +31,13 @@ Class UservoiceController extends AppControler {
         "expires" => date('Y-m-d', strtotime('+1 year')),
         "display_name" => $username,
         "url" => 'http://scratch.mit.edu/users/'.$this->getLoggedInUrlname(),
-        "avatar_url" => 'http://scratch.mit.edu/static/icons/buddy/'.$infoArr['guid'].'_med.png'
+        "avatar_url" => 'http://scratch.mit.edu/static/icons/buddy/'.$this->getLoggedInUserID().'_med.png'
       );
 
-
-      if $this->isAdmin():
+      if ($this->isAdmin())
          $user_data['admin'] = 'accept';
-      else:
+      else
          $user_data['admin'] = 'deny';
-
 
       $data = json_encode($user_data);
 
