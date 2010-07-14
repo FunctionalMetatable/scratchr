9e3e3b044d82913549465d4e767872c20a3d74da
syamantics <syamantics@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-07-14 21:09:28 +0000

    adding mexico locale static files

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -61,7 +61,7 @@ $this->params['webservices']
  define("NUM_MIN_SCRIPT_FOR_TOP_REMIX", 1);
  define("NUM_MIN_SCRIPT_FOR_TOP_LOVED", 1);
  //List of country for custumizable for home page.
- define("CUSTOMIZABLE_COUNTRIES", "IL");
+ define("CUSTOMIZABLE_COUNTRIES", "IL,MX");
  define("DEFAULT_COUNTRY", "worldwide");
 /**
  * Email Address Related to Flagging
