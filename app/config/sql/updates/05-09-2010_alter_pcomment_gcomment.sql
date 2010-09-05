a4327ad4befc222c5c9239dd3bc19002c1ca8d64
vineetkumar <vineetkumar@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-09-05 12:53:09 +0000

    test #442

---
+++ app/config/sql/updates/05-09-2010_alter_pcomment_gcomment.sql
@@ -0,0 +1,3 @@
+ ALTER TABLE `pcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment', 'parentcommentcensored', 'hiddenduetourl' ) NOT NULL DEFAULT 'visible';
+
+ ALTER TABLE `gcomments` CHANGE `comment_visibility` `comment_visibility` ENUM( 'visible', 'delbyusr', 'delbyadmin', 'censbyadmin', 'censbycomm', 'suspended', 'oldinvis', 'delbyparentcomment', 'parentcommentcensored', 'hiddenduetourl' ) NOT NULL DEFAULT 'visible' ; 
