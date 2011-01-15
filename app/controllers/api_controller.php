8584175e5f023d37d73600679ec51a0920f212a1
vineetkumar <vineetkumar@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-01-15 10:37:06 +0000

    test #475:api documentation

--- app/controllers/api_controller.php
+++ app/controllers/api_controller.php
@@ -1,12 +1,20 @@
 <?php
+/*******************************************************************************/
+##Api Controller
+##Author :Ashok kumar Gond
+##Note:Result is encoded with php function rawurlencode().
+/*******************************************************************************/
 class ApiController extends AppController {
 
     var $uses = array('Project','User');
 
     /**
-     * Redirects to project page based on a project id
-     * @parm int $pid => project id
-     */
+	* Name:getproject
+	* This function takes project id as input and redirects to project page based on a project id
+	* Parameter: project id
+	* Example: http://scratch.mit.edu/api/getproject/1211743
+	* Output: it will redirect to http://scratch.mit.edu/projects/Dab1998/1211743
+    */
     function getproject($pid=null) {
         $this->autoRender=false;
         $this->Project->bindUser();
@@ -16,9 +24,12 @@ class ApiController extends AppController {
     }
 
     /**
-     * Prints the path of a project based on the id
-     * @parm int $pid => project id
-     */
+	* Name: getprojectpath
+	* This function takes project id as input and print the .sb file path of a project
+	* Parameter: project id
+	* Example: http://scratch.mit.edu/api/getprojectpath/1211743
+	* Output: /projects/Dab1998/1211743.sb
+	*/
     function getprojectpath($pid=null) {
         $this->autoRender=false;
         $this->Project->bindUser();
@@ -29,9 +40,11 @@ class ApiController extends AppController {
     }
 
     /** 
-     * Redirects to user page based on a user id
-     * @parm int $uid => project id
-     */
+    * This function takes user id as input and redirects to user’s my stuff page based on a user id.
+    * Parameter: user id.
+	* Example: http://scratch.mit.edu/api/getuser/139
+	* Output: it will redirect to http://scratch.mit.edu/users/andresmh/
+    */
     function getuser($uid=null) {
         $this->autoRender=false;
         $this->Project->bindUser();
@@ -41,9 +54,11 @@ class ApiController extends AppController {
     } 
 
     /** 
-     * Redirects to user page based on a user id
-     * @parm int $uid => project id
-     */
+	* This function takes user id as input and print username.
+	* Parameter: user id.
+	* Example: http://scratch.mit.edu/api/getusernamebyid/139
+	* Output: andresmh
+	*/
     function getusernamebyid($uid=null) {
         $this->autoRender=false;
         $this->Project->bindUser();
@@ -54,8 +69,11 @@ class ApiController extends AppController {
 
 	
 	/** 
-     * Returns all registered users
-     */
+    * This function returns total number of registered users.
+	* Parameter: none.
+	* Example: http://scratch.mit.edu/api/getregisteredusers
+	* Output: 682,139
+    */
 	function getregisteredusers(){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -77,8 +95,11 @@ class ApiController extends AppController {
     }
 	
 	/** 
-     * Returns the number of people who have crated a project.
-     */
+    * This function returns the number of people who have created a project.
+	* Parameter: none.
+	* Example: http://scratch.mit.edu/api/getcreators
+	* Output: 194,830
+    */
 	function getcreators(){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -95,8 +116,11 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns the total number of projects uploaded to the website
-     */
+    * This function returns the total number of projects uploaded by users.
+	* Parameter: none.
+	* Example: http://scratch.mit.edu/api/gettotalprojects
+	* Output: 1,484,878
+    */
 	function gettotalprojects(){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -113,8 +137,11 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns sum of totalScripts 
-     */
+    * This function returns sum of total Scripts.
+	* Parameter: none.
+	* Example: http://scratch.mit.edu/api/gettotalscripts
+	* Output: 25,806,007
+    */
 	function gettotalscripts(){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -131,8 +158,11 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns sum of numberOfSprites 
-     */
+    * This function returns sum of number of sprites.
+	* Parameter: none.
+	* Example: http://scratch.mit.edu/api/gettotalsprites.
+	* Output: 8,746,913
+    */
 	function gettotalsprites(){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -149,9 +179,11 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns all visible project of user
-	 @param username 
-     */
+    * This function returns all id of visible project separated with colon (:) created by an user.
+	* Parameter: username
+	* Example: http://scratch.mit.edu/api/getprojectsbyusername/ashok
+	* Output: 20805:920784:920767:920759:785614:715431:609769:608275:490870:490868:397124:371906
+    */
 	function getprojectsbyusername($username){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -173,9 +205,11 @@ class ApiController extends AppController {
 	
 	
 	/** 
-     * Returns all friends of user
-	 @param username 
-     */
+    * This function returns all friend id of an user separated with colon (:).
+	* Parameter: username
+	* Example: http://scratch.mit.edu/api/getfriendsbyusername/ashok
+	* Output: 273837:261892:140467:9811:183315:82753:184712:185632:183245:184333
+    */
 	function getfriendsbyusername($username){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -196,9 +230,11 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns all galleries of user
-	 @param username 
-     */
+    * This function returns all gallery id created by an user separated with colon (:).
+	* Parameter: username
+	* Example: http://scratch.mit.edu/api/getgalleriesbyusername/ashok
+	* Output: 27321:24994
+    */
 	function getgalleriesbyusername($username){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -219,9 +255,12 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns  user's info
-	 @param username 
-     */
+    * This function returns profile information of an user.
+	* Parameter: username
+	* Output: username: id: country
+	* Example: http://scratch.mit.edu/api/getinfobyusername/ashok
+	* Example output: ashok:139123:India
+    */
 	function getinfobyusername($username){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -242,9 +281,12 @@ class ApiController extends AppController {
 	
 	
 	/** 
-     * Returns  all projects of a gallery
-	 @param galleryid 
-     */
+    * This function returns all projects (creator  and project id) of a gallery. Result contains one project details per line.
+	* Parameter: gallery id
+	* Output: project creator:project id
+	* Example: http://scratch.mit.edu/api/getprojectsbygallery/27321
+	* Example output: UnitedChuckVids:255443
+    */
 	function getprojectsbygallery($gallery_id){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -269,10 +311,17 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns project info
-	 @param project_id 
-	 results->user_id:name:description:created:tags:country:loveit:num_favoriters:remixer:remixes:numberOfSprites:totalScripts:numberofcomments:numberofdownloads 
-     */
+    * This function returns project information. Result contains one project details per line.
+	* Parameter: project id(one or more project ids)
+	* Output: author_id:name:description:created:tags:country:loveit:num_favoriters:remixer:remixes:numberOfSprites
+	:totalScripts:numberofcomments:numberofdownloads
+	* Example: http://scratch.mit.edu/api/getprojectinfobyid/1210197
+	* Example output:
+261892:Snake:Eat%20the%20fruits%2C%20and%20don%27t%20touch%20the%20walls%20or%20your%20own%20tail.%20%20Use%20the%20arrow%20keys%20to%20navigate.Get%20the%20Extra%20points%20as%20soon%20as%20u%20can.:2010-07-24%2007%3A32%3A08:game%2Cfruit%2Csnake:BD:7:3:14:15:4:9:31:24
+	Example:  http://scratch.mit.edu/api/getprojectinfobyid/785614/490868
+	Example output: 139123:3%20FishChomp%20remix:Try%20to%20make%20the%20big%20fish%20eat%20the%20smaller%20fish.%0D%0DINSTRUCTIONS%0DClick%20the%20Green%20Flag%20to%20start.%20Move%20the%20mouse%20to%20control%20the%20big%20fish.%0D%0DHOW%20I%20MADE%20THIS%0D%2A%20To%20see%20whether%20the%20big%20fish%20is%20close%20enough%20to%20eat%20the%20little%20fish%2C%20I%20used%20the%20%22color%20_%20is%20over%20_%3F%22%20block%0D%2A%20The%20little%20fish%20broadcasts%20%22got%20me%22%20when%20the%20big%20fish%20gets%20close%20to%20it%2C%20which%20triggers%20the%20big%20fish%20to%20animate%20its%20mouth%20in%20an%20eating%20motion.%0D%0DMORE%20IDEAS%0D%2A%20Keep%20score%20of%20how%20many%20fish%20are%20eaten%0D%2A%20Make%20different%20kinds%20of%20fish%20%28%22good%20fish%22%20and%20%22bad%20fish%22%29:2009-04-16%2002%3A31%3A45:::::1:1:5:5:2:0
+	139123:FISHVILLE:this%20is%20just%20a%20prctice%20project:2009-12-02%2000%3A27%3A17:::::0:0:4::0:0
+	*/
 	function getprojectinfobyid(){
 			Configure::write('debug', 0);
 			 $arg_list = array();
@@ -312,11 +361,16 @@ class ApiController extends AppController {
 		$this->Project->mc_close();
 		exit;
 	}
-	
-	/** 
-     * Returns user authentication
-	 @param username,password 
-     */
+	/**
+	* This function returns user information if success and  'false'. if authentication fails.
+	* Parameter: username,password
+	* Note: password should be encoded using rawurlencode() method.So if your password is “demo#123” you need to use password as
+	http://scratch.mit.edu/api/authenticateuser?username=demo&password=demo%23123
+	where "demo%23123" is rawurlencode format of "demo#123"
+	* Output:user_id:username:status
+	* Example:http://scratch.mit.edu/api/authenticateuser?username=demo&password=demo123 
+	* Example output: 12345:demo: normal
+	*/
 	function authenticateuser(){
 		Configure::write('debug', 0);
 		$username  = $_GET['username'];
@@ -341,9 +395,16 @@ class ApiController extends AppController {
 	
 	
 	/** 
-     * Returns gallery info
-	 @param gallery_id  one or more as parameter
-	 results:author:name:description:total project:usgae:staus,visibility:created
+    * This function returns gallery information. Result contains one gallery details per line.
+	* Parameter: gallery id(one or more gallery ids)
+	* Output: author_id:name:description:total project:usgae:staus,visibility:created
+
+	* Example: http://scratch.mit.edu/api/getgalleryinfobyid/24994 
+	* Example output: 139123:scratchr:http%3A%2F%2Fscratch.mit.edu%2Fgalleries%2Fview%2F24994:0:friends:notreviewed:visible:2008-07-25%2005%3A48%3A20 
+
+	* Example:  http://scratch.mit.edu/api/getgalleryinfobyid/24994
+	* Example output:  139123:scratchr:http%3A%2F%2Fscratch.mit.edu%2Fgalleries%2Fview%2F24994:0:friends:notreviewed:visible:2008-07-25%2005%3A48%3A20
+	139123:ashoks%20gallery:this%20is%20my%20gallery:1:private:notreviewed:visible:2008-08-25%2017%3A54%3A08
      */
 	function getgalleryinfobyid(){
 		Configure::write('debug', 0);
@@ -374,10 +435,16 @@ class ApiController extends AppController {
 		exit;
 	}
 	
-	/** 
-     * Returns number of project visible/all depends on parameter onlyvisible(yes/no)
-	 By default only visible projects
-     */
+	/**
+	* This function returns number of project visible/all depends on parameter onlyvisible(yes/no) created by user. By default  result contains only visible projects.
+	* Parameter: username
+	* Output: number of projects.
+	* Example:  http://scratch.mit.edu/api/getnumprojectsbyuser/ashok
+	* Example output: 12
+
+	* Example:  http://scratch.mit.edu/api/getnumprojectsbyuser/ashok?onlyvisible=’no’
+	* Example output: 12
+	*/
 	function getnumprojectsbyuser($username){
 		Configure::write('debug', 0);
 		$user_id = $this->User->field('id', array('User.username' => $username));
@@ -393,10 +460,14 @@ class ApiController extends AppController {
 	
 	
 	/** 
-     * Returns all visible pcomment
-	 @param pid
-	 results:user_id:createddate:comment
-     */
+    * This function returns all visible comments of a project. Result contains one comment details per line.
+	* Parameter: project id
+	* Output:author_id:comment_id:reply_to_comment_id,createddate:comment
+	* Example: http://scratch.mit.edu/api/getpcommentsbyid/255443 
+	* Example output: 
+144037:897255::2008-09-02%2012%3A26%3A46:cool%20n%20gr8%2C%20only%20i%20dont%20get%20the%20end
+155300:898467::2008-09-02%2019%3A02%3A48:he%20get%27s%20hurt%20then%20he%20blasts%20%27em%0D%0Aoh%20and%20sorry%20it%20was%2019%20not%2018%0D%0A%28school%20just%20started%20I%27m%20losing%20my%20mind%29
+	*/
 	function getpcommentsbyid ($pid){
 		Configure::write('debug', 0);
 		
@@ -429,9 +500,12 @@ class ApiController extends AppController {
 	}
 	
 	/** 
-     * Returns  all favoriteproject
-	 @param userid 
-     */
+    * This function returns all favorite  project id of user separated with colon(:)
+	* Parameter: user id
+	* Output:project_id:project_id
+	* Example: http://scratch.mit.edu/api/getusersfavoriteprojectsbyuid/261892 
+	* Example output: 766560:703044
+    */
 	function getusersfavoriteprojectsbyuid($uid=null){
 		Configure::write('debug', 0);
 		$this->Project->mc_connect();
@@ -452,9 +526,12 @@ class ApiController extends AppController {
 		exit;
 	}
 	
-	/*
-	*return the block count data of latest project version
-	*params project_id
+	/**
+	* This function returns block count code of latest version of the project (in json format)
+	* Parameter: project id
+	* Example: http://scratch.mit.edu/api/getprojectblockscount/4447
+	* Output: {"project_id":"4447","project_version":"2","scratchComment":"0","KeyEventHatMorph":"0","EventHatMorph_StartClicked":"3","EventHatMorph":"2","MouseClickEventHatMorph":"1","WhenHatBlockMorph":"0","and_operator":"0","multiply_operator":"0","add_operator":"0","subtract_operator":"0","divide_operator":"0","isLessThan":"0","isEqualTo":"0","isGreaterThan":"0","mod_operator":"0","or_operator":"0","abs":"0","allMotorsOff":"0","allMotorsOn":"0","answer":"0","append_toList_":"0","backgroundIndex":"0","bounceOffEdge":"0","broadcast_":"1","changeBackgroundIndexBy_":"0","changeBlurBy_":"0","changeBrightnessShiftBy_":"0","changeCostumeIndexBy_":"0","changeFisheyeBy_":"0","changeGraphicEffect_by_":"2","changeHueShiftBy_":"0","changeMosaicCountBy_":"0","changePenHueBy_":"0","changePenShadeBy_":"0","changePenSizeBy_":"0","changePixelateCountBy_":"0","changePointillizeSizeBy_":"0","changeSaturationShiftBy_":"0","changeSizeBy_":"0","changeStretchBy_":"0","changeTempoBy_":"0","changeVar_by_":"0","changeVisibilityBy_":"0","changeVolumeBy_":"0","changeWaterRippleBy_":"0","changeWhirlBy_":"0","changeXposBy_":"0","changeYposBy_":"2","clearPenTrails":"0","color_sees_":"0","comeToFront":"0","comment_":"0","computeFunction_of_":"0","concatenate_with_":"0","contentsOfList_":"0","costumeIndex":"0","deleteLine_ofList_":"0","distanceTo_":"0","doAsk":"0","doBroadcastAndWait":"0","doForever":"3","doForeverIf":"0","doIf":"0","doIfElse":"0","doPlaySoundAndWait":"0","doRepeat":"0","doReturn":"0","doUntil":"0","doWaitUntil":"0","drum_duration_elapsed_from_":"0","filterReset":"0","forward_":"0","getAttribute_of_":"0","getLine_ofList_":"0","glideSecs_toX_y_elapsed_from_":"0","goBackByLayers_":"0","gotoSpriteOrMouse_":"0","gotoX_y_":"1","gotoX_y_duration_elapsed_from_":"0","heading":"0","heading_":"1","hide":"2","hideVariable_":"0","insert_at_ofList_":"0","isLoud":"0","keyPressed_":"0","letter_of_":"0","lineCountOfList_":"0","list_contains_":"0","lookLike_":"0","midiInstrument_":"0","motorOnFor_elapsed_from_":"0","mousePressed":"0","mouseX":"0","mouseY":"0","nextBackground":"0","nextCostume":"0","not":"0","noteOn_duration_elapsed_from_":"0","penColor_":"0","penSize_":"0","playSound_":"1","pointTowards_":"0","putPenDown":"0","putPenUp":"0","randomFrom_to_":"0","rest_elapsed_from_":"0","rewindSound_":"0","readVariable":"0","rounded":"0","say_":"3","say_duration_elapsed_from_":"0","sayNothing":"0","scale":"0","sensor_":"0","sensorPressed_":"0","setBlurTo_":"0","setBrightnessShiftTo_":"0","setFisheyeTo_":"0","setGraphicEffect_to_":"0","setHueShiftTo_":"0","setLine_ofList_to_":"0","setMosaicCountTo_":"0","setMotorDirection_":"0","setPenHueTo_":"0","setPenShadeTo_":"0","setPixelateCountTo_":"0","setPointillizeSizeTo_":"0","setSaturationShiftTo_":"0","setSizeTo_":"0","setStretchTo_":"0","setTempoTo_":"0","setVar_to_":"0","setVisibilityTo_":"0","setVolumeTo_":"0","setWaterRippleTo_":"0","setWhirlTo_":"0","show":"2","showBackground_":"0","showVariable_":"0","soundLevel":"0","sqrt":"0","stampCostume":"0","startMotorPower_":"0","stopAll":"0","stopAllSounds":"0","stringLength_":"0","tempo":"0","think_":"0","think_duration_elapsed_from_":"0","timer":"0","timerReset":"0","touching_":"0","touchingColor_":"0","turnAwayFromEdge":"0","turnLeft_":"0","turnRight_":"3","volume":"0","wait_elapsed_from_":"4","xpos":"0","xpos_":"0","yourself":"0","ypos":"0","ypos_":"0","askYahoo":"0","wordOfTheDay_":"0","jokeOfTheDay_":"0","synonym_":"0","info_fromZip_":"0","scratchrInfo_forUser_":"0","other":""}
+
 	*/
 	function getprojectblockscount($pid=null){
 		if(empty($pid)){
@@ -485,8 +562,11 @@ class ApiController extends AppController {
 	}//eof
 	
 	/*
-	*return the human_readable data of latest project version
-	*params project_id
+	* This function returns  human-readable code of the latest version of the project (in json format)
+	* Parameter: project id
+	* Example:  http://scratch.mit.edu/api/getprojectblocks/1308192 
+	* Output: {"project_id":"1308192","sprites":["","when I am clicked\n set \"arms\" to (pick random 4 to 20)\n set \"mini arm angle\" to (pick random 5 to 90)\n set \"mini arm length\" to (pick random 1 to 20)\n set \"mini arms\" to (pick random 1 to 4)\n set \"size\" to (pick random 10 to 150)\n set \"mainframe pen size\" to (pick random 1 to 5)\n set \"M.A. pen size\" to (pick random 1 to 5)\n\nwhen green flag clicked\n forever\n go to x: 0 y: -20\n if \n switch to costume \"costume2\"\n else\n switch to costume \"costume1\"\n\n","","when green flag clicked\n forever\n wait until \n wait until >\n go to x: 0 y: -20\n pen down\n clear\n point in direction 180\n set pen color to c[ebf7ff]\n repeat \"arms\"\n set pen size to \"mainframe pen size\"\n move (\"size\" \/ ((\"mini arms\" * 2) + 0.1)) steps\n repeat \"mini arms\"\n turn \"mini arm angle\" degrees\n set pen size to \"M.A. pen size\"\n move \"mini arm length\" steps\n move (0 - \"mini arm length\") steps\n turn (\"mini arm angle\" * 2) degrees\n move \"mini arm length\" steps\n move (0 - \"mini arm length\") steps\n turn \"mini arm angle\" degrees\n set pen size to \"mainframe pen size\"\n move (\"size\" \/ ((\"mini arms\" * 2) + 0.1)) steps\n go to x: 0 y: -20\n turn (360 \/ \"arms\") degrees\n\n","when green flag clicked\n go to x: 0 y: 0\n forever\n change y by -2\n wait 0 secs\n set x to 0\n if <(y position) < \"-318\">\n set y to 360\n\n","when green flag clicked\n go to x: 0 y: 360\n forever\n change y by -2\n wait 0 secs\n set x to 0\n if <(y position) < \"-318\">\n set y to 360\n\n"]}
+
 	*/
 	function getprojectblocks($pid=null){
 		if(empty($pid)){
