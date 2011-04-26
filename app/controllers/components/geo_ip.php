dd669952bbef42f3e8b1eb47766b6bbb7a11584f
vineetkumar <vineetkumar@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-04-26 04:16:05 +0000

    added comment for function in apiController and modified api function get_latest_project

--- app/controllers/components/geo_ip.php
+++ app/controllers/components/geo_ip.php
@@ -17,6 +17,18 @@ class GeoIpComponent extends Object {
 		$handle = geoip_open($path."GeoIP.dat", GEOIP_STANDARD);
 		return geoip_country_code_by_addr($handle, $ip);
 	}
+	/*
+	Returns full country name based on IP
+	@params:ip
+	*/
+	function lookupCountryName($ip){
+		$path  =  APP.'misc/';
+		App::import('Vendor', 'Example', array('file' => 'GeoIp'.DS.'geoip.inc'));
+		// read GeoIP database
+		$handle = geoip_open($path."GeoIP.dat", GEOIP_STANDARD);
+		return geoip_country_name_by_addr($handle, $ip);
+	}
+
     
     function findIp() {
       if(getenv("HTTP_CLIENT_IP"))
