bf9a7e4586d75ba5040f36eea88fcd59095539a5
tmickel <tmickel@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-07-19 22:12:45 +0000

    Adding Integraflag graph stats

--- app/models/integraflag.php
+++ app/models/integraflag.php
@@ -2,6 +2,32 @@
 class Integraflag extends AppModel
 {
     var $name = 'Integraflag';
+    
+    function findStatsByAction($start=null, $end=null) {
+      if(!$start) $start = date('Y-m-d', time()-(60*60*24*90));
+      if(!$end) $end = date('Y-m-d', time()+1000);
+      $sql = "SELECT UNIX_TIMESTAMP(DATE(`created`)) AS date, 
+                      `action`, 
+                      COUNT(*) AS count 
+              FROM `integraflags`
+              WHERE `created` > '$start' AND `created` <= '$end' 
+              GROUP BY DATE(`created`), `action`
+              ORDER BY `created`";
+      return $this->query($sql);
+    }
+    
+    function findStatsByType($start=null, $end=null) {
+      if(!$start) $start = date('Y-m-d', time()-(60*60*24*90));
+      if(!$end) $end = date('Y-m-d', time()+1000);
+      $sql = "SELECT UNIX_TIMESTAMP(DATE(`created`)) AS date,
+                      `type`,
+                      COUNT(*) AS count
+              FROM `integraflags`
+              WHERE `created` > '$start' AND `created` <= '$end'
+              GROUP BY DATE(`created`), `action`
+              ORDER BY `created`";
+      return $this->query($sql);
+    }
 }
 
 
